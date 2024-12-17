c2b714b02959faf13f47fd8582c8979b5d2e6b86
hayat118 <tariqueht6@gmail.com>
2024-12-17 16:33:50 +0530

    added story type option

--- app/create-story/_component/StoryType.tsx
+++ app/create-story/_component/StoryType.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
 
 function StoryType() {
-  return <div>StoryType</div>;
+  const [selectedOption, setSelectedOption] = useState<string>();
+
+  const OptionList = [
+    {
+      label: "Story Book",
+      imageUrl: "/story.png",
+      isFree: true,
+    },
+    {
+      label: "Bed  Story",
+      imageUrl: "/bed.png",
+      isFree: true,
+    },
+    {
+      label: "Educational",
+      imageUrl: "/education.png",
+      isFree: true,
+    },
+  ];
+
+  return (
+    <div>
+      <label className="font-bold text-2xl text-primary">2.Story Type</label>
+      <div className="grid grid-cols-3 gap-3 mt-3 ">
+        {OptionList.map((item, index) => (
+          <div
+            key={item.imageUrl}
+            className={`relative grayscale hover:grayscale-0 cursor-pointer p-2 ${
+              selectedOption == item.label ? "grayscale-0" : "grayscale"
+            }`}
+            onClick={() => setSelectedOption(item.label)}
+          >
+            <h2 className="absolute bottom-3 text-white text-center w-full ">
+              {item.label}
+            </h2>
+            <Image
+              src={item.imageUrl}
+              alt={item.label}
+              width={200}
+              height={300}
+              className="object-cover h-[200px] rounded-2xl"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default StoryType;
