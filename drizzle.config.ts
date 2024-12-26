2317936dc638c8a69daab45613b9439120ec39ff
hayat118 <tariqueht6@gmail.com>
2024-12-26 16:53:21 +0530

    back end setup with drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(
+  "Database Connection String:",
+  process.env.NEXT_PUBLIC_DATABASE_URL || "Not set"
+);
 
 export default defineConfig({
-  out: "./drizzle",
+  // out: "./drizzle",
   dialect: "postgresql",
   schema: "./config/schema.tsx",
-  driver: "pglite",
+  // driver: "pglite",
   dbCredentials: {
     url: "postgresql://ai-story-builder_owner:NLrTGh6zEb0m@ep-old-thunder-a5o9zql1.us-east-2.aws.neon.tech/ai-story-builder?sslmode=require",
   },
+  verbose: true,
+  strict: true,
 });
